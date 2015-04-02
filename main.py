3a0efcc7bb20546719ea9b3ec12337743fb47443
ryanderson00000 <ryan_anderson00000@hotmail.com>
2015-04-01 23:40:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1892,6 +1892,89 @@ def highercard(number):
 					highercard4=computertiecard(2,3)
 					if highercard1==highercard2 and highercard1!=highercard4:
 ####################################################Part 6####################################################
+						if highercard4>highercard1:
+							highercard4=4
+						else:
+							highercard4=1
+						####SUITS DECIDE########
+						highercard1=suit
+						highercard2=suit2
+						if highercard1>highercard2:
+							highercard1=3
+							highercard2=2
+						else:
+							highercard1=2
+							highercard2=3
+					if highercard1==highercard4 and highercard1!=highercard2:
+						if highercard2>highercard1:
+							highercard2=4
+						else:
+							highercard2=1
+						####SUITS DECIDE########
+						highercard1=suit
+						highercard4=suit4
+						if highercard1>highercard4:
+							highercard1=3
+							highercard4=2
+						else:
+							highercard1=2
+							highercard4=3
+					if highercard2==highercard4 and highercard2!=highercard1:
+						if highercard1>highercard2:
+							highercard1=4
+						else:
+							highercard1=1
+						####SUITS DECIDE########
+						highercard2=suit2
+						highercard4=suit4
+						if highercard2>highercard4:
+							highercard2=3
+							highercard4=2
+						else:
+							highercard2=2
+							highercard4=3
+					if highercard1==highercard2 and highercard1==highercard4:
+						####SUITS DECIDE########
+						highercard1=suit
+						highercard2=suit2
+						highercard4=suit4
+						if highercard1>highercard2 and highercard1>highercard4:
+							if highercard2>highercard4:
+								highercard1=4
+								highercard2=3
+								highercard4=2
+							if highercard4>highercard2:
+								highercard1=4
+								highercard2=2
+								highercard4=3
+						if highercard2>highercard1 and highercard2>highercard4:
+							if highercard1>highercard4:
+								highercard1=3
+								highercard2=4
+								highercard4=2
+							if highercard4>highercard1:
+								highercard1=2
+								highercard2=4
+								highercard4=3
+						if highercard4>highercard1 and highercard4>highercard2:
+							if highercard1>highercard2:
+								highercard1=3
+								highercard2=2
+								highercard4=4
+							if highercard2>highercard1:
+								highercard1=2
+								highercard2=3
+								highercard4=4
+		if highercard1==highercard3 and highercard1==highercard4 and highercard1!=highercard2:
+		#####1,3,4######
+			if highercard3>highercard1:
+				highercard3=5
+			else:
+				highercard3=0
+			highercard1=CardClicked2(1)
+			DeckCounter=DeckCounter-1
+			highercard3=computertiecard(1,1)
+#################################################part 7###############################################################################
 		if highercard1>highercard2 and highercard1>highercard3 and highercard1>highercard4:
 			if highercard2>highercard3 and highercard2>highercard4:
 				if highercard3>highercard4:
